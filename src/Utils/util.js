390263117201f2d2ca7a13a49861593945661c76
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-03-01 18:29:27 +0530

    fix:label created will be shown directly

--- src/Utils/util.js
+++ src/Utils/util.js
@@ -1,6 +1,7 @@
 export function createNewLabel(labelName, dispatch, setLabelName,setOpenTag) {
     if(labelName!==''){
       dispatch({ type: "CREATE_NEW_LABEL", payload: labelName });
+      dispatch({type:'SET_LABEL_TO_NOTE',payload:labelName})
       setLabelName("");
       setOpenTag(false)
     }
